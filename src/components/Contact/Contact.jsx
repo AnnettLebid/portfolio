075e3054fbc1e4b61e6da0e3e80a060ee8e9ac9c
Anna Lebid <annalez16@gmail.com>
2021-06-14 16:11:25 +0300

    update Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,10 @@
-import React, { useContext } from 'react';
+/* eslint-disable prettier/prettier */
+import React from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container } from 'react-bootstrap';
-import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
 
 const Contact = () => {
-  const { contact } = useContext(PortfolioContext);
-  const { cta, btn, email } = contact;
 
   return (
     <section id="contact">
@@ -15,15 +13,15 @@ const Contact = () => {
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
+              Would you like to work with me? Awesome!
             </p>
             <a
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
+              href="mailto:annettlebid@gmail.com"
             >
-              {btn || "Let's Talk"}
+              Let&apos;s Talk
             </a>
           </div>
         </Fade>
