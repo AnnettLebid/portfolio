ebfcbd3bdb9f5ee5dc13a93e21febc6d1879756a
Anna Lebid <annalez16@gmail.com>
2021-06-16 23:17:45 +0300

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,20 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Anna Lebid`,
+        short_name: `portfolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
         display: `standalone`,
-        icon: 'src/images/favicon.png',
+        icon: 'src/images/logo.png',
+        icons: [
+          {
+            src: `favicons/icon-48x48.png`,
+            sizes: `48x48`,
+            type: `image/png`,
+          },
+        ],
       },
     },
   ],
